d9178f5e9bb8830bd65c691c39e20a79ce672483
Stephen Pinto <spinto@g.hmc.edu>
2013-09-21 17:35:56 -0700

    cleaned up MATLAB code. Don't actually need to do two separate analyses for the BNC and 10x probe. Since we're doing the analysis based on their corresponding corner frequency, we can just do the analysis for a generic first order low pass filter and it will apply to both.

--- lab1_m_files/E153_1.m
+++ lab1_m_files/E153_1.m
@@ -1,38 +1,42 @@
-%%  BNC Analysis
+%% First order low pass filter analysis
 clear all
 
-fc = 1; % Hz. Arbitrary.
-f = fc;
+% Set an arbitrary corner frequency.
+% The corner frequency itself does not matter because we're doing our
+% analysis here based on the fact the filter drops off at -20 dB/decade
+% after the corner.
+fc = 1;
 
 % V_pp = 1 V, so amplitude = 0.5 V
 V_in = 0.5;
 
-t = linspace(0, 2/f, 1000); % Two periods of time data
+t = linspace(0, 2/fc, 1000); % Two periods of time data
 
-% Harmonics and corresponding gains to build the input waves using 
+% The following harmonics will be used to construct the waves using the
 % fourier series
 harmonics = (1:1:999)';
-input_gains = ones(999,1);
 
 %%% Input waves
-input_square = square_wave(t, V_in, f, harmonics, input_gains);
-input_triangle = triangle_wave(t, V_in, f, harmonics, input_gains);
+input_square = square_wave(t, V_in, fc, harmonics, ones(999,1));
+input_triangle = triangle_wave(t, V_in, fc, harmonics, ones(999,1));
 
 % The system is a low pass filter, meaning after the cutoff frequency the
-% response goes down at -20 dB/decade. This means the gain for any
+% FRF drops off at -20 dB/decade. This means the gain for any
 % frequency after the corner frequency is G = f_corner / f
 
-%%% Output waves at corner frequency
-fc_gains = (fc / f) ./ harmonics;
+% For an input at the corner frequency, the gain for each harmonic is
+% just the inverse of the harmonic
+fc_gains = 1 ./ harmonics;
 
-fc_square = square_wave(t, V_in, f, harmonics, fc_gains);
-fc_triangle = triangle_wave(t, V_in, f, harmonics, fc_gains);
+fc_square = square_wave(t, V_in, fc, harmonics, fc_gains);
+fc_triangle = triangle_wave(t, V_in, fc, harmonics, fc_gains);
 
-%%% Output waves at 10 times corner frequencyasdfas
-ten_fc_gains = (fc / (10 * f)) ./ harmonics;
+%%% For an input at 10 times the corner frequency, the gain for each
+%%% harmonic is 1/(10n)
+ten_fc_gains = 1 ./ (10 * harmonics);
 
-ten_fc_square = square_wave(t, V_in, f, harmonics, ten_fc_gains);
-ten_fc_triangle = triangle_wave(t, V_in, f, harmonics, ten_fc_gains);
+ten_fc_square = square_wave(t, V_in, fc, harmonics, ten_fc_gains);
+ten_fc_triangle = triangle_wave(t, V_in, fc, harmonics, ten_fc_gains);
 
 % Plot square waves
 figure(1)
@@ -46,8 +50,8 @@ plot(t, ten_fc_square, 'b')
 
 title('Square wave through low pass filter','FontSize',30)
 legend('Input','Output at corner freq.', 'Output at 10x corner freq.')
-axis([0 2/f -0.6 0.6])
-set(gca,'XTick',(1/f):(1/f),'XTickLabel','T','FontSize',30)
+axis([0 2/fc -0.6 0.6])
+set(gca,'XTick',(1/fc):(1/fc),'XTickLabel','T','FontSize',30)
 xlabel('time')
 set(gca,'YTick',0:V_in:V_in,'YTickLabel',['0';'A'],'FontSize',30)
 ylabel('Voltage')
@@ -64,7 +68,7 @@ plot(t, ten_fc_triangle, 'b')
 
 title('Triangle wave through low pass filter','FontSize',30)
 legend('Input','Output at corner freq.', 'Output at 10x corner freq.')
-set(gca,'XTick',(1/f):(1/f),'XTickLabel','T','FontSize',30)
+set(gca,'XTick',(1/fc):(1/fc),'XTickLabel','T','FontSize',30)
 xlabel('time')
 set(gca,'YTick',0:V_in:V_in,'YTickLabel',['0';'A'],'FontSize',30)
 ylabel('Voltage')
